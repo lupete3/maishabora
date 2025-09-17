bd3a46ea02241f5115abd88dbab63ef69f7b5b69
lupete3 <lupeteplacide@gmail.com>
2025-09-17 08:50:28 +0200

    Ajout retenu salaire

--- resources/views/livewire/cash/cash-closing-history.blade.php
+++ resources/views/livewire/cash/cash-closing-history.blade.php
@@ -48,10 +48,11 @@
                                 @endif
                             </td>
                             <td>
-                                @if (auth()->user()->role === 'caissier' || auth()->user()->role === 'admin' || auth()->user()->role === 'comptable' && $closing->status === 'pending')
+                                @if ((auth()->user()->role === 'caissier' && $closing->status === 'pending')
+                                    || (auth()->user()->role === 'admin' && $closing->status === 'pending')
+                                    || (auth()->user()->role === 'comptable' && $closing->status === 'pending'))
                                     <button wire:click="validateClosing({{ $closing->id }})"
                                         class="btn btn-success btn-sm">Valider</button>
-
                                     <button class="btn btn-danger btn-sm" data-bs-toggle="modal"
                                         data-bs-target="#rejectModal{{ $closing->id }}">Rejeter</button>
 
