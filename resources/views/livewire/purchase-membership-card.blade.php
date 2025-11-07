e0b78cf87c93d55062a132f6c1a4aedac02c5ae9
lupete3 <lupeteplacide@gmail.com>
2025-11-07 15:14:18 +0200

    Fix result search

--- resources/views/livewire/purchase-membership-card.blade.php
+++ resources/views/livewire/purchase-membership-card.blade.php
@@ -123,7 +123,7 @@
                                     <td>{{ optional($card->member)->code ?? 'N/A' }} {{ optional($card->member)->name ?? 'N/A' }}
                                         {{ optional($card->member)->postnom ?? 'N/A' }} {{ optional($card->member)->prenom ?? 'N/A' }}
                                     </td>
-                                    <td>{{ number_format($card->price, 2) }} {{ $card->currency }}</td>
+                                    <td>{{ number_format($card->price, 2) }} CDF</td>
                                     <td>{{ number_format($card->subscription_amount, 2) }} {{ $card->currency }}</td>
                                     <td>{{ \Carbon\Carbon::parse($card->start_date)->format('d/m/Y') }}</td>
                                     <td>{{ \Carbon\Carbon::parse($card->end_date)->format('d/m/Y') }}</td>
