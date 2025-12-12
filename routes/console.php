9dc76509bb49f6624982e5766d68b4484b4879fe
lupete3 <lupeteplacide@gmail.com>
2025-12-12 11:51:59 +0200

    Fix command cron

--- routes/console.php
+++ routes/console.php
@@ -8,6 +8,5 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::command('check:overdue-repayments')->dailyAt('23:59');
-// Schedule::command('check:overdue-repayments')->twiceDaily(1, 7);
+Schedule::command('check:overdue-repayments')->dailyAt('11:54');
 
