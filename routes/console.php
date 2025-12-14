548458981e3b23e5f4705e835f35eaf4aa07b308
lupete3 <lupeteplacide@gmail.com>
2025-12-15 00:15:48 +0200

    Fix tache cron

--- routes/console.php
+++ routes/console.php
@@ -8,5 +8,5 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::command('check:overdue-repayments')->dailyAt('11:54');
-
+Schedule::command('check:overdue-repayments')->dailyAt('21:00');
+Schedule::command('check:overdue-repayments')->dailyAt('22:00');
