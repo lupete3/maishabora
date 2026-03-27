f24a1ffe8209a24a3f13eef66eb203cc03e3b1ec
lupete3 <lupeteplacide@gmail.com>
2026-03-27 13:38:20 +0200

    Fix Design

--- resources/views/reports/ai-credit.blade.php
+++ resources/views/reports/ai-credit.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'Expert IA - Crédits')
 
 @section('content')
-    <div class="container-xxl flex-grow-1 container-p-y">
+    <div class="flex-grow-1">
         <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">IA /</span> Performance Crédit</h4>
         @livewire('reports.ai-credit-report')
     </div>
