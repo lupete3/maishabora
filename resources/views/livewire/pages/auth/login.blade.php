b1ee2eb5cc6fe9da8ae40341cc9665ab80081e92
lupete3 <lupeteplacide@gmail.com>
2026-08-09 20:16:00 +0200

    Redesign Google Auth Bouton

--- resources/views/livewire/pages/auth/login.blade.php
+++ resources/views/livewire/pages/auth/login.blade.php
@@ -179,8 +179,18 @@ new #[Layout('layouts.guest')] class extends Component
                     </div>
 
                     <div class="mb-3">
-                        <a href="{{ route('auth.google') }}" class="btn btn-outline-danger d-grid w-100">
-                            Connexion avec Google
+                        <a href="{{ route('auth.google') }}"
+                        class="btn btn-light border d-flex align-items-center justify-content-center w-100 py-2"
+                        style="height: 44px; font-weight: 500;">
+
+                            <svg width="20" height="20" viewBox="0 0 24 24" class="me-2">
+                                <path fill="#4285F4" d="M21.35 12.27c0-.78-.07-1.54-.22-2.27H12v4.3h5.24a4.48 4.48 0 0 1-1.94 2.94v2.45h3.14c1.84-1.69 2.91-4.18 2.91-7.42z"/>
+                                <path fill="#34A853" d="M12 21.75c2.63 0 4.84-.87 6.45-2.36l-3.14-2.45c-.87.58-1.98.92-3.31.92-2.54 0-4.69-1.72-5.46-4.03H3.3v2.53A9.75 9.75 0 0 0 12 21.75z"/>
+                                <path fill="#FBBC05" d="M6.54 13.83A5.86 5.86 0 0 1 6.23 12c0-.64.11-1.26.31-1.83V7.64H3.3A9.75 9.75 0 0 0 2.25 12c0 1.57.38 3.05 1.05 4.36l3.24-2.53z"/>
+                                <path fill="#EA4335" d="M12 6.14c1.43 0 2.71.49 3.72 1.45l2.79-2.79C16.83 3.25 14.63 2.25 12 2.25A9.75 9.75 0 0 0 3.3 7.64l3.24 2.53C7.31 7.86 9.46 6.14 12 6.14z"/>
+                            </svg>
+
+                            <span>Continuer avec Google</span>
                         </a>
                     </div>
 
